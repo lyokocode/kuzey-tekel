328403f44e10639cfea7ca87e1fb6a854a49c9b8
lyokocode <aelita.dev@gmail.com>
2023-09-06 06:12:34 +0300

    contact page UI 2.0

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet"
 import "../styles/contact.scss"
+import { BsFacebook } from "react-icons/bs"
 
 export const Contact = () => {
     return (
@@ -18,26 +19,30 @@ export const Contact = () => {
                 />
                 <meta
                     name="keywords"
-                    content=" alkol, bira, viski, vodka, rakı, şarap"
+                    content=" alkol, bira, viski, vodka, rakı, şarap, bornova, tekel, izmir bornova"
                 />
                 <meta name="robots" content="index, follow" />
             </Helmet>
+
             {/* contact page */}
             <div className="contactDetails">
                 <div className="leftDetails">
-                    <h2>İletişim Bilgileri</h2>
-                    <p>Sosyal Medya Hesaplarımız:</p>
+                    <h1>İletişim Bilgileri</h1>
                     <ul>
-                        <li>Facebook: <a href="#">@kuzeytekel</a></li>
-                        <li>Instagram: <a href="#">@kuzeytekel</a></li>
-                        {/* Diğer sosyal medya hesapları */}
+                        <li>
+                            <h3>Facebook:</h3> <a href="#">@kuzeytekel</a>
+                        </li>
+                        <li>
+                            <h3>Instagram:</h3> <a href="#">@kuzeytekel</a>
+                        </li>
+
                     </ul>
-                    <p>Telefon Numaramız: <a href="tel:0123456789">0123 456 78 9</a></p>
-                    <p>E-posta Adresimiz: <a href="mailto:info@kuzeytekel.com">info@kuzeytekel.com</a></p>
+                    <p>Telefon Numaramız: <a href="tel:+905524794249">0552 479 42 49</a></p>
+                    <p>E-posta: <a href="mailto:info@kuzeytekel.com">info@kuzeytekel.com</a></p>
                 </div>
-                <div className="rightMap">
+                <div className="rightMap  ">
                     {/* maps embed code */}
-                    <iframe src="https://maps.google.com/maps?q=ergene%20555%20sokak%20no:16&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="100%" height="450" frameBorder="0" style={{ border: 0 }} allowFullScreen=""></iframe>
+                    <iframe className=" h-[100%]" src="https://maps.google.com/maps?q=ergene%20555%20sokak%20no:16&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="100%" height="450" frameBorder="0" style={{ border: 0 }} allowFullScreen=""></iframe>
                 </div>
             </div>
         </div>
